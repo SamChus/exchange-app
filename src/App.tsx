d4ea54d3573ab150b34021793a8d201e1676c04c
Samuel <samuelchigo55@gmail.com>
2024-01-09 23:35:26 +0100

    checked tradeview-widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import TradeView from "./TradeView";
 
 function App() {
-  const [value, setValue] = useState<number>(0);
-  useEffect(() => {
-    const ws = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
-
-     ws.onmessage = (event) => {
-      setValue(parseFloat(JSON.parse(event.data).p));
-     
-    };
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>{value}</h1>
-    </div>
-  );
+  return <TradeView />;
 }
 
 export default App;
+
+
